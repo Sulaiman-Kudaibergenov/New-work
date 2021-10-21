9c372cbb877d89677f24d7edcf468c19aba282bc
Sulaiman Kudaibergenov <59912956+Sulaiman-Kudaibergenov@users.noreply.github.com>
2021-10-21 14:24:25 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h2>HTML Image</h2>
  Hello world!
  Solomon first web-page
-<img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600">
+<img src="Без название.jpeg" alt="Girl in a jacket" width="500" height="600">
 
 </body>
 </html>
